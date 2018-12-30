b50421962c22e2b32afa3acbf6367b2b10b545bb
Amit Patil <iamitpatil1993@gmail.com>
2018-12-30 10:09:37 +0530

    Chap-5:Defining class-level request handling

--- src/main/java/com/example/mvc/web/HomeController.java
+++ src/main/java/com/example/mvc/web/HomeController.java
@@ -16,14 +16,16 @@ import org.springframework.web.bind.annotation.RequestMethod;
 			// case, it did not worked.
 			// NOTE: Using @Controller annotation give more information about type of component it is. So, we should always prefer to use annotations that will
 			// give more information about typr of component it is for example, @Controller, @Repository etc. over normal @Component
+@RequestMapping(value = "/") // This will be shared by all methods in this controllers.
 public class HomeController {
 
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 
 	/**
 	 * Simply selects the home view to render by returning its name.
+	 * This will handle GET - /
 	 */
-	@RequestMapping(value = "/", method = RequestMethod.GET)
+	@RequestMapping(method = RequestMethod.GET)
 	public String home() {
 		logger.info("Inside home controller...");
 		return "home";
