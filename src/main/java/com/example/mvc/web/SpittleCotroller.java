beeb1d328981ec775211592c2f866b808e50b1e3
Amit Patil <iamitpatil1993@gmail.com>
2019-01-24 08:56:59 +0530

    Chap-6:Todo-26:Send custom validation errors to view

--- src/main/java/com/example/mvc/web/SpittleCotroller.java
+++ src/main/java/com/example/mvc/web/SpittleCotroller.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -117,6 +118,7 @@ public class SpittleCotroller {
 	public String register(@Valid @ModelAttribute(name = "spittle") Spittle spittle, BindingResult validationErrors, Model model) {
 		logger.info("Insidr register ...");
 		if (validationErrors.hasErrors()) {
+			validationErrors.addError(new ObjectError("spittle", "Neet bharana bhau form")); // this how we can add custom/object level/irrespective of any field add custom validation error message. It will be carried to view along with other validation errors in same manneer.
 			return "spittle/register"; // we can't simply redirect, if we want to populate form again with submitted details, so we need to put submitted spittle object into model and just need to render the view
 		}
 		spittleRepository.create(spittle);
